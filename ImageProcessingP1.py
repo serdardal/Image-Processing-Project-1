914d700bcf51a9c0a88172a47ca2f4c6ed51d1a1
Serdar Dal <serdardal@users.noreply.github.com>
2019-10-28 22:56:15 +0300

    hessian, canny, prewitt, laplace, sato filtreleri eklendi

--- ImageProcessingP1.py
+++ ImageProcessingP1.py
@@ -9,7 +9,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
-from skimage import data, io, filters, img_as_ubyte
+from skimage import data, io, filters, img_as_ubyte, feature
 from skimage.color import rgb2gray, gray2rgb
 from scipy.misc import imsave
 
@@ -99,6 +99,7 @@ class MerkeziWidget(QWidget):
             self.etiket.setPixmap(pix)
             
             self.acikResim = dosyaYolu[0]
+        print(dosyaYolu)
             
     def dosyaKaydet(self):
         dosyaYolu = QFileDialog.getSaveFileName(self,"Kayıt Yeri Seç",str(datetime.timestamp(datetime.now())), "*.jpg *.png")
@@ -107,39 +108,112 @@ class MerkeziWidget(QWidget):
             io.imsave(dosyaYolu[0],self.islenmis)
             
     def filtreUI(self):
-        secim = QComboBox()
-        secim.addItem("Sobel")
+        self.secim = QComboBox()
+        self.secim.addItems(["Sobel","Hessian","Canny","Prewitt","Laplace","Sato"])
         
         uygulaButon = QPushButton("Uygula")
         uygulaButon.resize(80,30)
         uygulaButon.clicked.connect(self.filtreUygula)
         
         form = QFormLayout()
-        form.addRow("Filtre Seçimi: ",secim)
+        form.addRow("Filtre Seçimi: ",self.secim)
         form.addRow(uygulaButon)
         self.filtreTab.setLayout(form)
     
     def filtreUygula(self):
         resim = io.imread(self.acikResim)
         
-        #sobel filtresi için 2d arraya dönüştürülmesi
+        #filtreler için 2d arraya dönüştürülmesi
         gri = rgb2gray(resim)
         
-        filtrelenmis = filters.sobel(gri)
-        
-        self.islenmis = filtrelenmis
-        
-        #filtreleme float64 tipinde 2d array çıktı veriyor ekrana basmak için bize uint8 tipinde 3d array(rgb) gerekli
-        donusturulmus = gray2rgb(img_as_ubyte(filtrelenmis))
-        
-        #np arrayin QImage dönüştürülmesi
-        height, width, channel = donusturulmus.shape
-        bytesPerLine = 3 * width
-        qImg = QImage(donusturulmus.data, width, height, bytesPerLine, QImage.Format_RGB888)
-        
-        pixmap = QPixmap(qImg)
-        pix = pixmap.scaled(400, 600, Qt.KeepAspectRatio)
-        self.etiket.setPixmap(pix)
+        index = self.secim.currentIndex()
+        if index == 0:
+            
+            filtrelenmis = filters.sobel(gri)
+            
+            self.islenmis = filtrelenmis
+            
+            #filtreleme float64 tipinde 2d array çıktı veriyor ekrana basmak için bize uint8 tipinde 3d array(rgb) gerekli
+            donusturulmus = gray2rgb(img_as_ubyte(filtrelenmis))
+            
+            #np arrayin QImage dönüştürülmesi
+            height, width, channel = donusturulmus.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(donusturulmus.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            
+            pixmap = QPixmap(qImg)
+            pix = pixmap.scaled(400, 600, Qt.KeepAspectRatio)
+            self.etiket.setPixmap(pix)
+            
+        elif index == 1:
+            filtrelenmis = filters.hessian(gri)
+            self.islenmis = filtrelenmis
+            
+            donusturulmus = gray2rgb(img_as_ubyte(filtrelenmis))   
+            
+            height, width, channel = donusturulmus.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(donusturulmus.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            
+            pixmap = QPixmap(qImg)
+            pix = pixmap.scaled(400, 600, Qt.KeepAspectRatio)
+            self.etiket.setPixmap(pix)
+            
+        elif index == 2:
+            filtrelenmis = feature.canny(gri)
+            self.islenmis = filtrelenmis
+            
+            donusturulmus = gray2rgb(img_as_ubyte(filtrelenmis))   
+            
+            height, width, channel = donusturulmus.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(donusturulmus.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            
+            pixmap = QPixmap(qImg)
+            pix = pixmap.scaled(400, 600, Qt.KeepAspectRatio)
+            self.etiket.setPixmap(pix)
+            
+        elif index == 3:
+            filtrelenmis = filters.prewitt(gri)
+            self.islenmis = filtrelenmis
+            
+            donusturulmus = gray2rgb(img_as_ubyte(filtrelenmis))   
+            
+            height, width, channel = donusturulmus.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(donusturulmus.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            
+            pixmap = QPixmap(qImg)
+            pix = pixmap.scaled(400, 600, Qt.KeepAspectRatio)
+            self.etiket.setPixmap(pix)
+            
+        elif index == 4:
+            filtrelenmis = filters.laplace(resim)
+            self.islenmis = filtrelenmis
+            
+            filtrelenmis = img_as_ubyte(filtrelenmis)
+            
+            height, width, channel = filtrelenmis.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(filtrelenmis.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            
+            pixmap = QPixmap(qImg)
+            pix = pixmap.scaled(400, 600, Qt.KeepAspectRatio)
+            self.etiket.setPixmap(pix)
+            
+        elif index == 5:
+            filtrelenmis = filters.sato(gri)
+            self.islenmis = filtrelenmis
+            
+            donusturulmus = gray2rgb(img_as_ubyte(filtrelenmis))   
+            
+            height, width, channel = donusturulmus.shape
+            bytesPerLine = 3 * width
+            qImg = QImage(donusturulmus.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            
+            pixmap = QPixmap(qImg)
+            pix = pixmap.scaled(400, 600, Qt.KeepAspectRatio)
+            self.etiket.setPixmap(pix)
 
 
     
